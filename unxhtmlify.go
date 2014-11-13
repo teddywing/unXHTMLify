a5e49ca0ba59da7ae9ea304c9c44f4ae0f325444
Teddy Wing <git@teddywing.com>
2014-11-13 02:39:35 -0500

    Initial commit. Copy stdin to stdout.
    Copy the code from here:
    http://golang.org/pkg/bufio/#example_Scanner_lines
    
    to read stdin line by line and print it back to stdout.

---
+++ unxhtmlify.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "reading standard input", err)
+	}
+}
